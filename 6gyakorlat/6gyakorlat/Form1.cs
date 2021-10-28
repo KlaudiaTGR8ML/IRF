3893dec48e943900c96e2e94b94ab3952f9f57df
Klaudia Szabó <klaudia.szabo1@stud.uni-corvinus.hu>
2021-10-28 16:47:01 +0200

    Adatok megjelenítése diagramon

--- 6gyakorlat/6gyakorlat/Form1.cs
+++ 6gyakorlat/6gyakorlat/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace _6gyakorlat
@@ -22,7 +23,8 @@ namespace _6gyakorlat
             InitializeComponent();
             GetExchangeRates();
             GetXMLfeldolgozása();
-            dataGridView1.DataSource = Rates.ToString();
+            GetDiagram();
+            dataGridView1.DataSource = Rates;
             
         }
         private void GetExchangeRates()
@@ -45,27 +47,40 @@ namespace _6gyakorlat
             var xml = new XmlDocument();
             xml.LoadXml(result);
 
-            // Végigmegünk a dokumentum fő elemének gyermekein
             foreach (XmlElement element in xml.DocumentElement)
             {
-                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
-                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+
                 var rate = new RateData();
                 Rates.Add(rate);
 
-                // Dátum
                 rate.Date = DateTime.Parse(element.GetAttribute("date"));
-
-                // Valuta
+               
                 var childElement = (XmlElement)element.ChildNodes[0];
                 rate.Currency = childElement.GetAttribute("curr");
 
-                // Érték
                 var unit = decimal.Parse(childElement.GetAttribute("unit"));
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+            
+        }
+        private void GetDiagram()
+        {
+            chartRateData.DataSource = Rates;
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
