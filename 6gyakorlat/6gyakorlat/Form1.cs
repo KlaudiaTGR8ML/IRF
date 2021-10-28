595bd1cac7ada53064928a4f0a59277d66aac3ca
Klaudia Szabó <klaudia.szabo1@stud.uni-corvinus.hu>
2021-10-28 16:42:08 +0200

    XML feldolgozása

--- 6gyakorlat/6gyakorlat/Form1.cs
+++ 6gyakorlat/6gyakorlat/Form1.cs
@@ -9,17 +9,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace _6gyakorlat
 {
     public partial class Form1 : Form
     {
         BindingList<RateData> Rates = new BindingList<RateData>();
+        string result;
         public Form1()
         {
             InitializeComponent();
             GetExchangeRates();
+            GetXMLfeldolgozása();
             dataGridView1.DataSource = Rates.ToString();
+            
         }
         private void GetExchangeRates()
         {
@@ -33,8 +37,35 @@ namespace _6gyakorlat
         var response = mnbService.GetExchangeRates(request);
 
 
-        var result = response.GetExchangeRatesResult;
+         result = response.GetExchangeRatesResult;
 
+        }
+        private void GetXMLfeldolgozása()
+        {
+            var xml = new XmlDocument();
+            xml.LoadXml(result);
+
+            // Végigmegünk a dokumentum fő elemének gyermekein
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                // Létrehozzuk az adatsort és rögtön hozzáadjuk a listához
+                // Mivel ez egy referencia típusú változó, megtehetjük, hogy előbb adjuk a listához és csak később töltjük fel a tulajdonságait
+                var rate = new RateData();
+                Rates.Add(rate);
+
+                // Dátum
+                rate.Date = DateTime.Parse(element.GetAttribute("date"));
+
+                // Valuta
+                var childElement = (XmlElement)element.ChildNodes[0];
+                rate.Currency = childElement.GetAttribute("curr");
+
+                // Érték
+                var unit = decimal.Parse(childElement.GetAttribute("unit"));
+                var value = decimal.Parse(childElement.InnerText);
+                if (unit != 0)
+                    rate.Value = value / unit;
+            }
         }
     }
 }
